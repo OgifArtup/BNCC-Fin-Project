8a7f79eedb56c6fd5ef23c8b7b4701e4836a8f88
OgifArtup <58365011+OgifArtup@users.noreply.github.com>
2023-04-07 14:27:53 +0700

    Removed role from user

--- BNCC-Fin-Project/resources/views/login.blade.php
+++ BNCC-Fin-Project/resources/views/login.blade.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 <body>
-<div class="formBox">
+    <div class="formBox">
         <h1>Login</h1>
         <div class="mb-3">
             <label for="Email">Email</label>
@@ -30,5 +31,6 @@
         <a href="/">Don't have an account yet? Register</a>
         <button type="submit" class="loginBttn">Login</button>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 </body>
 </html>
